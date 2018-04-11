33db22c85dd4e741b2d7718bfce20db0ff14e885
Jay Desai <janhudesai@gmail.com>
2018-04-11 00:37:57 -0700

    API call for the Get User Data updated

--- Clipper/routes/texiservice.js
+++ Clipper/routes/texiservice.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 var ejs = require("ejs");
 
+var axios = require ('axios');
+
 
 router.get('/', function(req, res) {
   console.log("Inside the texiservice");
 
   console.log("req.query.No:"+req.query.No)
-
+  
+  if(req.query.No == "1" || req.query.No == "0"){
   ejs.renderFile('./views/contact.ejs',{data:req.query.No},function(err,result){
     if (!err) {
      
@@ -15,6 +18,29 @@ router.get('/', function(req, res) {
     }
   
 });
+}
+
+else if(req.query.No == "2"){
+
+    // API Call for the get user info 
+
+    var payload = {
+        "Id": 5,
+        "Accountstype":"Uber",
+        "Clippercarnumber":0
+    }
+    axios.post('http://localhost:3001/getUserData',payload)
+        .then(function(response){
+            console.log("response.data"+response.data)
+            ejs.renderFile('./views/contact.ejs',{data:req.query.No},function(err,result){
+            if (!err) {
+                    res.status(200).send(result);
+                    }
+    
+  });
+
+})
+  }
 });
 
 module.exports = router;
