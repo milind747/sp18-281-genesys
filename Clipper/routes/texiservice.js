1111eab6d924cec66f85eb1b48e75315932936b6
Jay Desai <janhudesai@gmail.com>
2018-04-10 23:43:58 -0700

    Routes for texiservices updated

--- Clipper/routes/texiservice.js
+++ Clipper/routes/texiservice.js
@@ -5,7 +5,8 @@ var ejs = require("ejs");
 
 router.get('/', function(req, res) {
   console.log("Inside the texiservice");
-  ejs.renderFile('./views/contact.ejs',function(err,result){
+
+  ejs.renderFile('./views/contact.ejs',{data:req.query.No},function(err,result){
     if (!err) {
      
         res.status(200).send(result);
