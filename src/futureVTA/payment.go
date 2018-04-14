36c82b6f333830652e9049e64e8c9151ab2eb482
Raj Baraiya <rajbaraiyaa@gmail.com>
2018-04-14 16:01:56 -0700

    code for parsing form and fetching query string

--- src/futureVTA/payment.go
+++ src/futureVTA/payment.go
@@ -17,10 +17,21 @@ import (
 func newParent(formatter *render.Render) http.HandlerFunc{
 	return func(w http.ResponseWriter, req *http.Request) {
 
+	//parsing json file	
+	req.ParseForm()
+	var user User
+	_ = json.NewDecoder(req.Body).Decode(&user)
 	//code for database connection with mongo in local
 	database := Database{"localhost", "cmpe281", nil}
 	data := &database
 	Connect(data)
+	//collection
+	c := data.db.C("payment")
+	
+	//need parent id for my api which i took from query string
+	var p Payment
+	i, err := strconv.Atoi(req.FormValue("parentid"))
+	err = c.Find(bson.M{"id":i}).One(&p)
 
 	}
 }
