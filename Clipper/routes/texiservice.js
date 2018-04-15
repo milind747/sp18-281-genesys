33feb4800bf6be643d5f800e626d112dbcb67c0e
Jay Desai <janhudesai@gmail.com>
2018-04-15 15:34:05 -0700

    Callback for Update User Profile function updated

--- Clipper/routes/texiservice.js
+++ Clipper/routes/texiservice.js
@@ -29,24 +29,41 @@ else if(req.query.No == "2"){
     console.log("req.query.password:"+req.query.password)
     
     
-    
-
-    var payload = {
-        "Id": 5,
-        "Accountstype":"Uber",
-        "Clippercarnumber":0
+    var user_details = {
+        "Id": 11,
+        "Accounts":{
+                "Type": "Uber",
+                "Username": req.query.username,
+                "Clippercarnumber": parseInt(req.query.clippercard),
+                "Password": req.query.password,
+                "Payment": 0,
+                "Noofrides":0
+        }
     }
-    axios.post('http://localhost:3001/getUserData',payload)
-        .then(function(response){
-            console.log("response.data"+response.data)
-            ejs.renderFile('./views/contact.ejs',{data:req.query.No,details:response.data},function(err,result){
-            if (!err) {
-                    res.status(200).send(result);
-                    }
-    
-  });
 
-})
+    axios.post('http://localhost:3001/updateUserData',user_details)
+        .then(function(response){
+            console.log("User Data Updated")
+
+            var payload = {
+                "Id": 11,
+                "Accountstype":"Uber",
+                "Clippercarnumber":parseInt(req.query.clippercard)
+            }
+            axios.post('http://localhost:3001/getUserData',payload)
+                .then(function(response){
+                    console.log("response.data"+response.data)
+                    ejs.renderFile('./views/contact.ejs',{data:req.query.No,details:response.data},function(err,result){
+                    if (!err) {
+                            res.status(200).send(result);
+                            }
+            
+          });
+        
+        })
+            })
+
+    
   }
 });
 
