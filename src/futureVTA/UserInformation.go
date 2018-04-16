22f86cfad0b7812d88ac5df9cf9fbd5d2db50086
milind747 <milindpatel3012@gmail.com>
2018-04-15 17:45:27 -0700

    changed struct and added import

--- src/futureVTA/UserInformation.go
+++ src/futureVTA/UserInformation.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"net/http"
+	"log"
+	"fmt"
 	"github.com/unrolled/render"
+	"gopkg.in/mgo.v2/bson"
 	)
 
 
 type UserDetails struct {
-	uid		string		
-	username	string		
-	parentid	string		
-	phone		string		
-	email		string		
+	_id 		bson.ObjectId 	`bson:"_id" json:"id"`
+	UID		string		`bson:"uid" json:"uid"`
+	USERNAME	string		`bson:"username" json:"username"`
+	PARENTID	string		`bson:"parentid" json:"parentid"`
+	PHONE		string		`bson:"phone" json:"phone"`
+	EMAIL		string		`bson:"email" json:"email"`
 }
 
