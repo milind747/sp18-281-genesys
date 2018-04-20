3e081023ae1579b647d7bb2d872caed96527e6cd
divyang8842 <divyang8842@gmail.com>
2018-04-19 19:45:37 -0700

    added database reusability

--- src/futureVTA/QRCode.go
+++ src/futureVTA/QRCode.go
@@ -29,6 +29,55 @@ type QRCodeUse struct {
 }
 
 
+func generateQRCode(formatter *render.Render) http.HandlerFunc{
+	return func(w http.ResponseWriter, req *http.Request) {
+		err := req.ParseForm();
+		if  err != nil {
+		        log.Fatal("form")
+		        log.Fatal(err)
+		}
+    		//data, err := json.Marshal(req.Form)
+    		//if err != nil {
+		//        log.Fatal("marshal")
+		//        log.Fatal(err)
+    		//}
+		//log.Println(data)
+
+		user := new(User)
+	       	//if err = json.Unmarshal(data, user); err != nil {
+        	//	log.Fatal("unmarshal")
+    		//}
+
+		user.UID = req.FormValue("uid")
+		user.PARENTID = req.FormValue("parentid")
+
+		database := Database{hostname,databaseName, nil}
+		Connect(&database);
+		qrdataC :=  (&database).db.C("qrcode")
+		current_time := time.Now()
+		var png []byte
+		previous := QRCodeStruct{}
+		checkIfQRCodeGenerated(user.UID,user.PARENTID,&previous)
+		if(len(previous.USETIMES) == 0){
+			var useArry []*QRCodeUse
+			png, err = qrcode.Encode("https://example.org?uid="+user.UID+"&parentid="+user.PARENTID, qrcode.Medium, 256)
+			if err != nil {
+				log.Fatal(err)
+			}
+			previous = QRCodeStruct{bson.NewObjectId(), user.UID,user.PARENTID,png,current_time,useArry}
+			err = qrdataC.Insert(&previous)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		}
+
+		formatter.JSON(w, http.StatusOK, previous)
+
+	}
+}
+
+
 func checkIfQRCodeGenerated(uid string,parentid string,result *QRCodeStruct){
 	database := Database{hostname, databaseName, nil}
 	Connect(&database);
