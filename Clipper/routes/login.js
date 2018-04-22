09a8f9ccf6d587d93eb8b9a16c47362f3a99b45f
nsjethani <neha.jethani6627@gmail.com>
2018-04-21 20:40:52 -0700

    home page for login

---
+++ Clipper/routes/login.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var ejs = require("ejs");
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    console.log("Neha")
+    var data=req.query.No;
+    
+    console.log("data:", data)
+    ejs.renderFile('./views/login.ejs',{data:data},function(err,result){
+        console.log(err)
+        if (!err) {
+            res.end(result);
+        }
+
+    });
+});
+
+module.exports = router;
